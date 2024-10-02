911640301c527a4ba4fdfc8318eb0362e477c93b
mohd abid <abidmohd763@gmail.com>
2024-10-02 23:23:45 +0530

    brand

---
+++ resources/views/admin/brands/brands.blade.php
@@ -0,0 +1,88 @@
+@extends('admin.layouts.master')
+@section('content')
+<!-- Content wrapper scroll start -->
+<div class="content-wrapper-scroll">
+
+    <!-- Content wrapper start -->
+    <div class="content-wrapper">
+
+        <!-- Row start -->
+        <div class="row">
+            <div class="col-sm-12 col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="card-title">Brands</div>
+                        <div class="ml-auto">
+                            <a href="{{ route('brands/add') }}" class="btn btn-success">
+                                <i class="bi bi-plus"></i>Brand
+                            </a>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table v-middle m-0" id="laravel_datatable">
+                                <thead>
+                                    <tr>
+                                        <th>Sr. No</th>
+                                        <th>Name</th>
+                                        <th>Slug</th>
+                                        <th>Status</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Row end -->
+    </div>
+    <!-- Content wrapper end -->
+</div>
+
+<script src="{{ asset('assets/admin/js/main.js') }}"></script>
+<link href="https://cdn.datatables.net/1.11.4/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+<script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.11.4/js/dataTables.bootstrap5.min.js"></script>
+<!-- Content wrapper scroll end -->
+<script type="text/javascript">
+$(document).ready(function() {
+    $('#laravel_datatable').DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: "{{ route('brands') }}",
+        columns: [{
+                data: 'DT_RowIndex',
+                name: 'DT_RowIndex',
+                orderable: false,
+                searchable: false
+            },
+            {
+                data: 'name',
+                name: 'name'
+            },
+            {
+                data: 'slug',
+                name: 'slug'
+            },
+            {
+                data: 'status',
+                name: 'status'
+            },
+            {
+                data: 'action',
+                name: 'action',
+                orderable: false,
+                searchable: false
+            },
+        ],
+        language: {
+            emptyTable: "Data not available"
+        }
+    });
+});
+</script>
+<!-- Main Js Required -->
+<script src="{{ asset('assets/admin/js/main.js') }}"></script>
+@endsection
---
+++ resources/views/admin/sub_categories/sub_categories_edit.blade.php
@@ -0,0 +1,109 @@
+@extends('admin.layouts.master')
+@section('content')
+<div class="col-sm-12 col-12">
+    <div class="card">
+        <div class="card-header">
+            <div class="card-title">Edit Sub Categories</div>
+        </div>
+        <div class="card-body">
+            <form action="{{ route('sub_categories/update', $subCategories->id ) }}" method="POST">
+                @csrf
+                @method('PUT')
+                <div class="row">
+                    <div class="col-sm-12 col-12">
+                        <div class="card-border">
+                            <div class="card-border-title">Basic Information</div>
+                            <div class="card-border-body">
+
+                                <div class="row">
+                                    <div class="col-sm-6 col-12">
+                                        <div class="mb-4">
+                                            <label class="form-label">Category<span class="text-red">*</span></label>
+                                            <select name="category_id" class="form-control" id="category_id">
+                                                <option value="" disabled selected>Select Category</option>
+                                                @foreach($categories as $category)
+                                                <option value="{{ $category->id }}"
+                                                    {{ $category->id == $subCategories->category_id ? 'selected' : '' }}>
+                                                    {{ $category->name }}
+                                                </option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6 col-12">
+                                        <div class="mb-4">
+                                            <label class="form-label">Sub Category<span
+                                                    class="text-red">*</span></label>
+                                            <input type="text" name="name" id="name" value="{{ $subCategories->name }}"
+                                                class="form-control" placeholder="Enter Sub Category">
+
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6 col-12">
+                                        <div class="mb-3">
+                                            <label class="form-label">Slug<span class="text-red">*</span></label>
+                                            <input type="text" class="form-control" name="slug" id="slug"
+                                                value=" {{ $subCategories->slug }}" placeholder="Sub Category Slug"
+                                                readonly>
+
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6 col-12">
+                                        <div class="mb-3">
+                                            <label class="form-label">Status<span class="text-red">*</span></label>
+                                            <select class="form-control" name="status" id="subCategoryStatus">
+                                                <option value="Select Category Status">Select Status</option>
+                                                <option value="0" {{ $subCategories->status == 0 ? 'selected' :  ''}}>
+                                                    InActive
+                                                </option>
+                                                <option value="1" {{ $subCategories->status == 1 ? 'selected' : '' }}>
+                                                    Active</option>
+                                            </select>
+
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-12 col-12">
+                        <div class="custom-btn-group flex-end">
+                            <a href="{{ route('sub_categories') }}" class="btn btn-light">Cancel</a>
+                            <button type="submit" class="btn btn-success">Update</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<script>
+$('#name').change(function() {
+    element = $(this);
+
+    $.ajax({
+        url: "{{ route('get-slug') }}",
+        type: "GET",
+        data: {
+            title: element.val(),
+        },
+        dataType: "json",
+        success: function(response) {
+            if (response['status'] == true) {
+                $('#slug').val(response['slug']);
+            }
+        },
+        error: function(xhr, status, error) {
+            console.error(error);
+        }
+    });
+});
+</script>
+
+
+<!-- Main Js Required -->
+<script src="{{ asset('assets/admin/js/main.js') }}"></script>
+@endsection
